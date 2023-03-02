3ae108fd21402f22ec35c3e62c851ff2e2eb0570
Ilya Hasiuk <124878705+hepl-hasiuk@users.noreply.github.com>
2023-03-02 21:02:34 +0100

    Add files via upload

---
+++ GPIO/LED_et_Boutton2.py
@@ -0,0 +1,23 @@
+# HEPL-2/3/2023 
+# Allumer la LED
+# Hasiuk Ilya
+# 2/3/2023
+
+import machine
+import utime
+
+BUTTON = machine.Pin(16,machine.Pin.IN)
+LED = machine.Pin(18,machine.Pin.OUT)
+
+val = 0# création de variable initialisée à 1
+
+
+while True:
+    if BUTTON.value() == 1:#si on appui sur le boutton 
+        val = val+1 #on met val à 1 pour allumer la LED
+        utime.sleep(1)
+    elif val == 2:#mais dés que val vaut 2, ça veut dire qu'on a re-appuyé sur le boutton
+                  
+        val = 0   #alors on le met à zéro pour éteindre la LED
+        utime.sleep(1)
+    LED.value(val) # soit 1 soit 0 (ON ou OFF)
