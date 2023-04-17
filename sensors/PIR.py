4de63315697c8eb443ece07ff59d4e3dd048fcac
Ilya Hasiuk <124878705+hepl-hasiuk@users.noreply.github.com>
2023-04-17 09:18:23 +0200

    Update PIR.py

--- sensors/PIR.py
+++ sensors/PIR.py
@@ -1,21 +1,29 @@
+# Importation des modules nécessaires
 from machine import Pin
 from utime import sleep
 from servo import SERVO
 
-servo=SERVO(Pin(20))
-miniPir=Pin(18,Pin.IN)
-LED = machine.Pin(16,machine.Pin.OUT)
+# Initialisation de l'objet Servo sur la broche 20
+servo = SERVO(Pin(20))
 
+# Initialisation du capteur PIR sur la broche 18 en tant qu'entrée
+miniPir = Pin(18, Pin.IN)
+
+# Initialisation de la broche 16 en tant que sortie pour la LED
+LED = Pin(16, Pin.OUT)
+
+# Boucle infinie
 while True:
 
-    if miniPir.value()==1:
-        print('motion detected')
-        LED.value(1)
-        servo.turn(150)
-        sleep(1)
+    # Si le capteur PIR détecte un mouvement (valeur lue égale à 1)
+    if miniPir.value() == 1:
+        print('Mouvement détecté')
+        LED.value(1)  # Allumer la LED
+        servo.turn(150)  # Tourner le servo à 150 degrés
+        sleep(1)  # Attendre 1 seconde
 
-    else :
-        LED.value(0)
-        print('no movement ')
-        servo.turn(20)
-        sleep(1)
+    else:
+        LED.value(0)  # Éteindre la LED
+        print('Aucun mouvement')
+        servo.turn(20)  # Tourner le servo à 20 degrés
+        sleep(1)  # Attendre 1 seconde
