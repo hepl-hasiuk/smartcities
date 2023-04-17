7a807fae437769cb2bb3d81b52d3faf8700ba7df
Ilya Hasiuk <124878705+hepl-hasiuk@users.noreply.github.com>
2023-04-17 09:19:05 +0200

    Update servo.py

--- sensors/servo.py
+++ sensors/servo.py
@@ -1,8 +1,12 @@
+# Importation des modules nécessaires
 from machine import Pin, PWM
+
+# Définition d'une nouvelle classe appelée SERVO
 class SERVO:
     def __init__(self, pin):
-        self.pin = pin
-        self.pwm = PWM(self.pin)
+        self.pin = pin  # Initialisation de la broche du servo
+        self.pwm = PWM(self.pin)  # Création d'un objet PWM pour cette broche
+
     def turn(self, val):
-        self.pwm.freq(100)
-        self.pwm.duty_u16(int(val/180*13000+4000))
+        self.pwm.freq(100)  # Définition de la fréquence du signal PWM à 100 Hz
+        self.pwm.duty_u16(int(val/180*13000+4000))  # Définition du rapport cyclique du signal PWM en fonction de la valeur de val
